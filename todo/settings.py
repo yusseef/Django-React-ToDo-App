f4d75b5465d67ab717d81108e0d702a558b9ff98
yusseef <yussef.raouf11@gmail.com>
2021-04-13 16:55:16 +0200

    enable all cors

--- todo/settings.py
+++ todo/settings.py
@@ -126,9 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build/static')
